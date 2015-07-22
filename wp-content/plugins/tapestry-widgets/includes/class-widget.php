4e8c9050663406e4f03610dccd8b0e85d3efaa7a
Nigel M Peters <nutyga@gmail.com>
2015-07-22 15:33:39 +0000

    Added a new widget for CQC Standards

--- wp-content/plugins/tapestry-widgets/includes/class-widget.php
+++ wp-content/plugins/tapestry-widgets/includes/class-widget.php
@@ -34,14 +34,14 @@ class TAP_Widgets_Widget extends WP_Widget
 		$sb_title = $args['id'];
 		// Get the sidebar widgets array
 		$sidebars_widgets = wp_get_sidebars_widgets($sb_title);
-		
+
 		// Count the array
 		$this->sb_widget_count = count($sidebars_widgets[$args['id']]);
 		//echo 'Wid Counter: '.$this->widget_counter;
 		//echo 'Wid Count: '.$this->sb_widget_count;
 
 		$tapbox = "tap-box";
-		
+
 		// based on the current widget count, render the opening tag
 		if($this->sb_widget_count == 2 && $this->widget_counter == 0 && $sb_title != 'carousel_promos' || $this->sb_widget_count == 4 && $this->widget_counter == 0 && $sb_title != 'carousel_promos' )
 		{
@@ -56,9 +56,9 @@ class TAP_Widgets_Widget extends WP_Widget
 		{
 			echo '<div class="services-5">';
 		}
-										
-		extract( $args );		
-		
+
+		extract( $args );
+
 		$id = ($instance['tap-widget-id']) ? $instance['tap-widget-id'] : 0;
 		$color = $instance['color'];
 		$show_title = (isset($instance['show_title'])) ? $instance['show_title'] : 1;
@@ -66,7 +66,7 @@ class TAP_Widgets_Widget extends WP_Widget
 
 		// 5 Widgets need a special kind of setup, so here goes...
 		if($this->sb_widget_count == 5){
-			
+
 			switch ($this->widget_counter) {
 			    case 0:
 			        $before_widget = '<div class="column">'.$before_widget;
@@ -85,19 +85,19 @@ class TAP_Widgets_Widget extends WP_Widget
 			        $after_widget = '</div>'.$after_widget;
 			        break;
 			}
-			
+
 		}
 
 		$image =  get_the_post_thumbnail( $id, $tapbox );
 
 		echo $before_widget;
-	    
+
 
 		if(!empty($id) && $post) {
 
-			if($show_title) { 
+			if($show_title) {
 				// first check $instance['title'] so titles are not changes for people upgrading from an older version of the plugin
-				// titles WILL change when they re-save their widget.. 
+				// titles WILL change when they re-save their widget..
 				$title = (isset($instance['title'])) ? $instance['title'] : $post->post_title;
 				$title = apply_filters( 'widget_title', $title );
 			}
@@ -105,23 +105,23 @@ class TAP_Widgets_Widget extends WP_Widget
 			$content = apply_filters('ww_content', $post->post_content, $id);
 
 			?>
-			
+
 			<?php if($show_title) { echo $before_title . $title . $after_title; } ?>
 			<?php echo $this->category_label($post->ID) ?>
 			<?php echo '<a class="tapbox_overlay '.$color.'" href="'.get_permalink($id).'">&nbsp;</a>'; ?>
 			<?php if($image) { echo $this->remove_img_attr($image); } ?>
-			
+
 			<?php
 
 				} elseif(current_user_can('manage_options')) { ?>
 				<p>
-					<?php if(empty($id)) { 
+					<?php if(empty($id)) {
 						echo 'Please select a Tapestry Widget to show in this area.';
-					} else { 
+					} else {
 						echo 'No widget block found with ID %d, please select an existing Tapestry Widget in the widget settings.', 'tap-widgets';
 					} ?>
 				</p>
-		<?php 
+		<?php
 		}
 
 		echo $after_widget;
@@ -134,17 +134,17 @@ class TAP_Widgets_Widget extends WP_Widget
 		{
 			echo '</div>';
 		}
-				
+
 		// Reset the counter if we have cleared all the widgets for a sidebar
 		if(($this->widget_counter+1) != $this->sb_widget_count)
 		{
-			$this->widget_counter++;			
+			$this->widget_counter++;
 		}
 		else
 		{
 			$this->widget_counter = 0;
 		}
-		
+
 	}
 
 	// This is to stop the fixed height and widths distorting the images
@@ -154,15 +154,15 @@ class TAP_Widgets_Widget extends WP_Widget
 
 	// To create the category label
 	public function category_label($id){
-				
+
 		$tags = get_the_category($id);
-			
+
 		foreach ( $tags as $tag ) {
-			
+
 				$link = "<a href='{$tag_link}' title='{$tag->name} Tag' class='{$tag->slug} label label-blue'>{$tag->name}</a>";
-				return $link;		
+				return $link;
 		}
-				
+
 	}
 
 	/**
@@ -191,7 +191,7 @@ class TAP_Widgets_Widget extends WP_Widget
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		
+
 		$posts = (array) get_posts(array(
 			'post_type' => array('page'),
 			'numberposts' => -1
@@ -210,8 +210,8 @@ class TAP_Widgets_Widget extends WP_Widget
 
 		<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="hidden" value="<?php echo esc_attr( $title ); ?>" />
 
-		<p>	
-			<label for="<?php echo $this->get_field_id( 'tap-widget-id' ); ?>"><?php _e( 'Widget Block to show:', 'tap-widgets' ); ?></label> 
+		<p>
+			<label for="<?php echo $this->get_field_id( 'tap-widget-id' ); ?>"><?php _e( 'Widget Block to show:', 'tap-widgets' ); ?></label>
 			<select class="widefat" id="<?php echo $this->get_field_id('tap-widget-id'); ?>" name="<?php echo $this->get_field_name( 'tap-widget-id' ); ?>" required>
 				<option value="0" disabled <?php selected($selected_widget_id, 0); ?>><?php if(empty($posts)) { echo 'No widget blocks found'; } else { echo 'Select a widget block'; } ?></option>
 				<?php foreach($posts as $p) { ?>
@@ -244,3 +244,49 @@ class TAP_Widgets_Widget extends WP_Widget
 	}
 
 }
+
+/**
+ * Text widget class
+ *
+ * @since 2.8.0
+ */
+ class cqc_widget extends WP_Widget {
+
+
+     /** constructor -- name this the same as the class above */
+     function cqc_widget() {
+         parent::WP_Widget(false, $name = 'CQC Widget');
+     }
+
+     /** @see WP_Widget::widget -- do not rename this */
+     function widget($args, $instance) {
+         extract( $args );
+         $title 		= apply_filters('widget_title', $instance['title']);
+         $message 	= $instance['message'];
+         ?>
+					 <div class="tapbox-cqc">
+						 <script src="//www.cqc.org.uk/sites/all/modules/custom/cqc_widget/widget.js?data-id=1-1247119897&amp;data-host=www.cqc.org.uk" type="text/javascript"></script>
+					 </div>
+         <?php
+     }
+
+     /** @see WP_Widget::update -- do not rename this */
+     function update($new_instance, $old_instance) {
+ 		$instance = $old_instance;
+ 		$instance['title'] = strip_tags($new_instance['title']);
+ 		$instance['message'] = strip_tags($new_instance['message']);
+         return $instance;
+     }
+
+     /** @see WP_Widget::form -- do not rename this */
+     function form($instance) {
+
+         $title 		= esc_attr($instance['title']);
+         $message	= esc_attr($instance['message']);
+         ?>
+          <p>This adds the CQC widget with correctly formatted styles</p>
+         <?php
+     }
+
+
+ } // end class example_widget
